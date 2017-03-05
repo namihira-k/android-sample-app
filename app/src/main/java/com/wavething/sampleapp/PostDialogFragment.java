4f07765bb6e4d7de90368991c2382d10a08d9851
namihira-k <kosuke.namihira@gmail.com>
2017-03-05 11:40:43 +0900

    add dialog for post

---
+++ app/src/main/java/com/wavething/sampleapp/PostDialogFragment.java
@@ -0,0 +1,30 @@
+package com.wavething.sampleapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Dialog for POST
+ */
+public class PostDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.input_text)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        return builder.create();
+    }
+
+}
